a471c75801a055a56e458ad1c6fd806bf99f7a47
eduardo <edniemeyer@gmail.com>
2020-08-28 14:51:02 -0300

    feat: endpoint to retrieve merged flights + cache

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import timeout from "connect-timeout";
+import FlightsApi from "./api/flights";
+import cache from "./utils/cache";
 
 const app = express();
 
+const api = new FlightsApi();
+
 app.use(timeout('1s'))
 
-app.get("/", (req, res) => {
-  res.send("Hi!");
+app.get("/", cache(30), (req, res) => {
+  api.getMergedFlights()
+    .then(mergedFlights => {
+      if (req.timedout) return;
+      return res.send(JSON.stringify([...mergedFlights.values()]));
+    })
+
 });
 
 
